07638d8b2b082e25cb2e953a02b23c37496f1fdd
nakamura hiroki <nakamura2465@gmail.com>
2021-04-12 08:01:37 +0900

    チャプター81まで

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, SnapshotAction } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+
+import { Comment } from './class/comment';
+import { User } from './class/user';
+import { map } from 'rxjs/operators';
+
+const CURRENT_USER: User = new User(1,'中村　拡幹');
+const ANOTHER_USER: User = new User(2,'竹井　賢治');
 
 @Component({
   selector: 'ac-root',
@@ -6,5 +15,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments$: Observable<Comment[]>;
+  commentsRef: AngularFireList<Comment>;
+  currentUser = CURRENT_USER;
+  comment='';
+  item$: Observable<any>;
+
+  constructor(private db: AngularFireDatabase){
+    this.item$ = db.object('/item').valueChanges();
+    this.commentsRef = db.list('/comments');
+    this.comments$ = this.commentsRef.snapshotChanges()
+      .pipe(
+        map((snapshots: SnapshotAction<Comment>[]) =>{
+          return snapshots.map(snapshot =>{
+            const value = snapshot.payload.val();
+            return new Comment({ key: snapshot.payload.key,...value})
+          });
+        })
+      );
+  }
+
+
+  addComment(comment: string): void{
+    if(comment){
+      this.commentsRef.push(new Comment({ user: this.currentUser, message: comment }));
+      this.comment = '';
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyA7o9pgPYhNguBCeUiU9ADFof9VACLltzY",
+    authDomain: "angular-chat-13736.firebaseapp.com",
+    projectId: "angular-chat-13736",
+    storageBucket: "angular-chat-13736.appspot.com",
+    messagingSenderId: "767957865008",
+    appId: "1:767957865008:web:a82ec53989b2bedd5b93f5"
+  }
 };
 
 /*
